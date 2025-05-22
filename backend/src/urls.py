710b5278231577b2972e2006e9dc449b0a131417
franmorooliacci <franmorooliacci@gmail.com>
2025-05-22 12:07:49 -0300

    feat(adopcion): add adopcion urls

--- backend/src/urls.py
+++ backend/src/urls.py
@@ -2,7 +2,14 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import (
     TokenObtainPairView, TokenRefreshView)
-from .views import ResponsableViewSet, AnimalViewSet, RazaViewSet, EfectorViewSet, AtencionViewSet, InsumoViewSet, DomicilioViewSet, AtencionInsumoViewSet, ProfesionalViewSet, CustomTokenObtainPairView, ExternalDataViewSet
+from .views import (
+    ResponsableViewSet, AnimalViewSet, RazaViewSet,
+    EfectorViewSet, AtencionViewSet, InsumoViewSet,
+    DomicilioViewSet, AtencionInsumoViewSet, ProfesionalViewSet,
+    CustomTokenObtainPairView, ExternalDataViewSet, CandidatoViewSet,
+    InstitucionViewSet, InstitucionAnimalViewSet, PeticionAdopcionViewSet,
+    AdopcionViewSet, AdopcionFotoViewSet
+)
 
 router = DefaultRouter()
 router.register('responsables', ResponsableViewSet, basename='responsable')
@@ -16,6 +23,14 @@ router.register('atencion_insumo', AtencionInsumoViewSet,
                 basename='atencion_insumo')
 router.register('profesionales', ProfesionalViewSet, basename='profesional')
 router.register('external_data', ExternalDataViewSet, basename='external_data')
+router.register('candidatos', CandidatoViewSet, basename='candidato')
+router.register('instituciones', InstitucionViewSet, basename='institucion')
+router.register('peticiones', PeticionAdopcionViewSet, basename='peticion')
+router.register('institucion_animales',
+                InstitucionAnimalViewSet, basename='institucion_animal')
+router.register('adopcion_fotos', AdopcionFotoViewSet,
+                basename='adopcion_foto')
+router.register('adopciones', AdopcionViewSet, basename='adopcion')
 
 urlpatterns = [
     path('', include(router.urls)),
