18b7dc99c984d60f9cb4951e4e67a5482c88f7c2
franmorooliacci <franmorooliacci@gmail.com>
2025-06-13 11:35:53 -0300

    fix: rename static files path

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -12,7 +12,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 STATICFILES_DIRS = [
-    BASE_DIR.parent / 'frontend' / 'build' / 'static',
+    BASE_DIR.parent / 'frontend' / 'build',
 ]
 
 SECRET_KEY = config('DJANGO_SECRET_KEY')
