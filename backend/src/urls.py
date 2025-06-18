b8583fbeab08ba47d0af1c97bc8f3bd842b77bb7
franmorooliacci <franmorooliacci@gmail.com>
2025-06-18 11:38:56 -0300

    feat: add pdf/ route

--- backend/src/urls.py
+++ backend/src/urls.py
@@ -2,7 +2,12 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import (
     TokenObtainPairView, TokenRefreshView)
-from .views import ResponsableViewSet, AnimalViewSet, RazaViewSet, EfectorViewSet, AtencionViewSet, InsumoViewSet, DomicilioViewSet, AtencionInsumoViewSet, ProfesionalViewSet, CustomTokenObtainPairView, ExternalDataViewSet
+from .views import (
+    ResponsableViewSet, AnimalViewSet, RazaViewSet, 
+    EfectorViewSet, AtencionViewSet, InsumoViewSet, 
+    DomicilioViewSet, AtencionInsumoViewSet, ProfesionalViewSet, 
+    CustomTokenObtainPairView, ExternalDataViewSet, PdfApiView
+)
 
 router = DefaultRouter()
 router.register('responsables', ResponsableViewSet, basename='responsable')
@@ -20,5 +25,7 @@ router.register('external_data', ExternalDataViewSet, basename='external_data')
 urlpatterns = [
     path('', include(router.urls)),
     path('token/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh')
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('pdf/', PdfApiView.as_view(), name='pdf'),
+
 ]
