dfefe2a84e00a8d433662a4116aa2a455032fc91
franmorooliacci <franmorooliacci@gmail.com>
2025-06-23 12:13:24 -0300

    feat: add tamaño, color and animal_color models; refactor: rearrange models order

--- backend/src/models.py
+++ backend/src/models.py
@@ -2,73 +2,6 @@ from django.db import models
 from django.conf import settings
 
 
-class Animal(models.Model):
-    id = models.AutoField(primary_key=True)
-    nombre = models.CharField(max_length=255)
-    sexo = models.CharField(max_length=1)
-    año_nacimiento = models.IntegerField()
-    pelaje_color = models.CharField(max_length=255, blank=True, null=True)
-    id_responsable = models.ForeignKey(
-        'Responsable', models.DO_NOTHING, db_column='id_responsable', blank=True, null=True)
-    id_especie = models.ForeignKey(
-        'Especie', models.DO_NOTHING, db_column='id_especie')
-    id_raza = models.ForeignKey('Raza', models.DO_NOTHING, db_column='id_raza')
-    fallecido = models.IntegerField()
-
-    class Meta:
-        managed = False
-        db_table = 'animal'
-
-
-class Atencion(models.Model):
-    id = models.AutoField(primary_key=True)
-    id_efector = models.ForeignKey(
-        'Efector', models.DO_NOTHING, db_column='id_efector')
-    id_responsable = models.ForeignKey(
-        'Responsable', models.DO_NOTHING, db_column='id_responsable')
-    id_domicilio_responsable = models.ForeignKey(
-        'Domicilio', models.DO_NOTHING, db_column='id_domicilio_responsable', blank=True, null=True)
-    id_animal = models.ForeignKey(
-        Animal, models.DO_NOTHING, db_column='id_animal')
-    id_servicio = models.ForeignKey(
-        'Servicio', models.DO_NOTHING, db_column='id_servicio')
-    id_profesional = models.ForeignKey(
-        'Profesional', models.DO_NOTHING, db_column='id_profesional')
-    señas_particulares = models.CharField(
-        max_length=255, blank=True, null=True)
-    observaciones_animal = models.CharField(
-        max_length=255, blank=True, null=True)
-    fecha_ingreso = models.DateField(blank=True, null=True)
-    hora_ingreso = models.TimeField(blank=True, null=True)
-    fecha_egreso = models.DateField(blank=True, null=True)
-    hora_egreso = models.TimeField(blank=True, null=True)
-    estado_sanitario_egreso = models.CharField(
-        max_length=255, blank=True, null=True)
-    observaciones_atencion = models.CharField(
-        max_length=255, blank=True, null=True)
-    estado = models.IntegerField()
-
-    class Meta:
-        managed = False
-        db_table = 'atencion'
-
-
-class AtencionInsumo(models.Model):
-    id = models.AutoField(primary_key=True)
-    id_atencion = models.ForeignKey(
-        Atencion, models.DO_NOTHING, db_column='id_atencion')
-    id_insumo = models.ForeignKey(
-        'Insumo', models.DO_NOTHING, db_column='id_insumo')
-    cant_ml = models.IntegerField(blank=True, null=True)
-    cant_ml_prequirurgico = models.IntegerField(blank=True, null=True)
-    cant_ml_induccion = models.IntegerField(blank=True, null=True)
-    cant_ml_quirofano = models.IntegerField(blank=True, null=True)
-
-    class Meta:
-        managed = False
-        db_table = 'atencion_insumo'
-
-
 class Domicilio(models.Model):
     id = models.AutoField(primary_key=True)
     calle = models.CharField(max_length=255)
@@ -98,6 +31,96 @@ class Domicilio(models.Model):
         db_table = 'domicilio'
 
 
+class Responsable(models.Model):
+    id = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=255)
+    apellido = models.CharField(max_length=255)
+    dni = models.IntegerField()
+    sexo = models.CharField(max_length=1)
+    fecha_nacimiento = models.DateField(blank=True, null=True)
+    id_domicilio_renaper = models.ForeignKey(
+        Domicilio, models.DO_NOTHING, db_column='id_domicilio_renaper', related_name='domicilio_renaper', blank=True, null=True)
+    id_domicilio_actual = models.ForeignKey(
+        Domicilio, models.DO_NOTHING, db_column='id_domicilio_actual', related_name='domicilio_actual')
+    telefono = models.CharField(max_length=15, blank=True, null=True)
+    mail = models.CharField(max_length=255, blank=True, null=True)
+    firma = models.TextField(null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'responsable'
+
+
+class Especie(models.Model):
+    id = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=255)
+
+    class Meta:
+        managed = False
+        db_table = 'especie'
+
+
+class Raza(models.Model):
+    id = models.AutoField(primary_key=True)
+    id_especie = models.ForeignKey(
+        Especie, models.DO_NOTHING, db_column='id_especie')
+    nombre = models.CharField(max_length=255)
+
+    class Meta:
+        managed = False
+        db_table = 'raza'
+
+
+class Color(models.Model):
+    id = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=255)
+
+    class Meta:
+        managed = False
+        db_table = 'color'
+
+
+class Tamaño(models.Model):
+    id = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=255)
+
+    class Meta:
+        managed = False
+        db_table = 'tamaño'
+
+
+class Animal(models.Model):
+    id = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=255)
+    sexo = models.CharField(max_length=1)
+    fecha_nacimiento = models.DateField(blank=True, null=True)
+    id_tamaño = models.ForeignKey(
+        Tamaño, models.DO_NOTHING, db_column='id_tamaño', blank=True, null=True)
+    id_responsable = models.ForeignKey(
+        Responsable, models.DO_NOTHING, db_column='id_responsable', blank=True, null=True)
+    id_especie = models.ForeignKey(
+        Especie, models.DO_NOTHING, db_column='id_especie')
+    id_raza = models.ForeignKey('Raza', models.DO_NOTHING, db_column='id_raza', blank=True, null=True)
+    fallecido = models.IntegerField()
+    esterilizado = models.IntegerField()
+    adoptado_imusa = models.IntegerField()
+    colores = models.ManyToManyField(Color, through='AnimalColor')
+
+    class Meta:
+        managed = False
+        db_table = 'animal'
+
+
+class AnimalColor(models.Model):
+    id = models.AutoField(primary_key=True)
+    id_animal = models.ForeignKey(Animal, models.DO_NOTHING, db_column='id_animal')
+    id_color = models.ForeignKey(Color, models.DO_NOTHING, db_column='id_color')
+
+    class Meta:
+        managed = False
+        db_table = 'animal_color'
+
+
 class Efector(models.Model):
     id = models.AutoField(primary_key=True)
     nombre = models.CharField(max_length=255)
@@ -121,24 +144,13 @@ class Efector(models.Model):
         db_table = 'efector'
 
 
-class Especie(models.Model):
+class Servicio(models.Model):
     id = models.AutoField(primary_key=True)
     nombre = models.CharField(max_length=255)
 
     class Meta:
         managed = False
-        db_table = 'especie'
-
-
-class Insumo(models.Model):
-    id = models.AutoField(primary_key=True)
-    descripcion = models.CharField(max_length=255)
-    tope_max = models.IntegerField(blank=True, null=True)
-    tope_min = models.IntegerField(blank=True, null=True)
-
-    class Meta:
-        managed = False
-        db_table = 'insumo'
+        db_table = 'servicio'
 
 
 class Profesional(models.Model):
@@ -154,8 +166,8 @@ class Profesional(models.Model):
     firma = models.TextField(null=True)
     legajo = models.IntegerField()
     estado = models.IntegerField()
-    user = models.OneToOneField(
-        settings.AUTH_USER_MODEL, on_delete=models.DO_NOTHING, null=True)
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.DO_NOTHING, null=True)
+    efectores = models.ManyToManyField(Efector, through='ProfesionalEfector')
 
     class Meta:
         managed = False
@@ -175,46 +187,64 @@ class ProfesionalEfector(models.Model):
         db_table = 'profesional_efector'
 
 
-class Raza(models.Model):
-    id = models.AutoField(primary_key=True)
-    id_especie = models.ForeignKey(
-        Especie, models.DO_NOTHING, db_column='id_especie')
-    nombre = models.CharField(max_length=255)
-    tamaño = models.CharField(max_length=10)
-    limite_inf_altura_cm = models.IntegerField(blank=True, null=True)
-    limite_sup_altura_cm = models.IntegerField(blank=True, null=True)
-    limite_inf_peso_kg = models.IntegerField(blank=True, null=True)
-    limite_sup_peso_kg = models.IntegerField(blank=True, null=True)
-
-    class Meta:
-        managed = False
-        db_table = 'raza'
-
-
-class Responsable(models.Model):
+class Insumo(models.Model):
     id = models.AutoField(primary_key=True)
     nombre = models.CharField(max_length=255)
-    apellido = models.CharField(max_length=255)
-    dni = models.IntegerField()
-    sexo = models.CharField(max_length=1)
-    fecha_nacimiento = models.DateField(blank=True, null=True)
-    id_domicilio_renaper = models.ForeignKey(
-        Domicilio, models.DO_NOTHING, db_column='id_domicilio_renaper', related_name='domicilio_renaper', blank=True, null=True)
-    id_domicilio_actual = models.ForeignKey(
-        Domicilio, models.DO_NOTHING, db_column='id_domicilio_actual', related_name='domicilio_actual')
-    telefono = models.CharField(max_length=15, blank=True, null=True)
-    mail = models.CharField(max_length=255, blank=True, null=True)
-    firma = models.TextField(null=True)
+    tope_max = models.IntegerField(blank=True, null=True)
+    tope_min = models.IntegerField(blank=True, null=True)
 
     class Meta:
         managed = False
-        db_table = 'responsable'
+        db_table = 'insumo'
 
 
-class Servicio(models.Model):
+class Atencion(models.Model):
     id = models.AutoField(primary_key=True)
-    descripcion = models.CharField(max_length=255)
+    id_efector = models.ForeignKey(
+        Efector, models.DO_NOTHING, db_column='id_efector')
+    id_responsable = models.ForeignKey(
+        Responsable, models.DO_NOTHING, db_column='id_responsable')
+    id_domicilio_responsable = models.ForeignKey(
+        Domicilio, models.DO_NOTHING, db_column='id_domicilio_responsable', blank=True, null=True)
+    id_animal = models.ForeignKey(
+        Animal, models.DO_NOTHING, db_column='id_animal')
+    id_servicio = models.ForeignKey(
+        Servicio, models.DO_NOTHING, db_column='id_servicio')
+    id_profesional = models.ForeignKey(
+        Profesional, models.DO_NOTHING, db_column='id_profesional')
+    peso_kg = models.FloatField(blank=True, null=True)
+    señas_particulares = models.CharField(
+        max_length=255, blank=True, null=True)
+    observaciones_animal = models.CharField(
+        max_length=255, blank=True, null=True)
+    fecha_ingreso = models.DateField(blank=True, null=True)
+    hora_ingreso = models.TimeField(blank=True, null=True)
+    fecha_egreso = models.DateField(blank=True, null=True)
+    hora_egreso = models.TimeField(blank=True, null=True)
+    estado_sanitario_egreso = models.CharField(
+        max_length=255, blank=True, null=True)
+    observaciones_atencion = models.CharField(
+        max_length=255, blank=True, null=True)
+    estado = models.IntegerField()
+    insumos = models.ManyToManyField(Insumo, through='AtencionInsumo')
 
     class Meta:
         managed = False
-        db_table = 'servicio'
+        db_table = 'atencion'
+
+
+class AtencionInsumo(models.Model):
+    id = models.AutoField(primary_key=True)
+    id_atencion = models.ForeignKey(
+        Atencion, models.DO_NOTHING, db_column='id_atencion')
+    id_insumo = models.ForeignKey(
+        Insumo, models.DO_NOTHING, db_column='id_insumo')
+    cant_ml = models.IntegerField(blank=True, null=True)
+    cant_ml_prequirurgico = models.IntegerField(blank=True, null=True)
+    cant_ml_induccion = models.IntegerField(blank=True, null=True)
+    cant_ml_quirofano = models.IntegerField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'atencion_insumo'
+
