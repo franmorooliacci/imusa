d65fb1e825ab0699c2a3fa121605f049fad53bcd
franmorooliacci <franmorooliacci@gmail.com>
2025-06-25 12:50:42 -0300

    feat: add new fields to validate

--- frontend/src/validation/animalSchema.js
+++ frontend/src/validation/animalSchema.js
@@ -1,27 +1,48 @@
 import * as yup from 'yup';
 
 export const animalSchema = yup.object({
-    id_raza: yup
-        .string()
-        .required('Seleccione una raza'),
     nombre: yup
         .string()
         .required('Ingrese el nombre'),
+
     sexo: yup
         .string()
-        .oneOf(['M', 'H'], 'Seleccione el sexo')
-        .required('El sexo es requerido'),
-    año_nacimiento: yup
-        .number()
-        .typeError('Ingrese un año válido')
-        .integer('Ingrese un año entero')
-        .min(1900, 'Año demasiado bajo')
-        .max(new Date().getFullYear(), 'Año en el futuro')
-        .required('Ingrese el año de nacimiento'),
-    pelaje_color: yup
+        .oneOf(['M', 'H'], 'Seleccione el sexo'),
+
+    fecha_nacimiento: yup
+        .date()
+        .nullable(),
+
+    id_tamaño: yup
         .string()
-        .max(30, 'Máximo 30 caracteres'),
+        .required('Seleccione el tamaño'),
+
+    id_raza: yup
+        .string()
+        .required('Seleccione la raza'),
+
     fallecido: yup
         .number()
-        .oneOf([0, 1]),
+        .oneOf([0, 1])
+        .required(),
+
+    esterilizado: yup
+        .number()
+        .oneOf([0, 1])
+        .required(),
+
+    adoptado_imusa: yup
+        .number()
+        .oneOf([0, 1])
+        .required(),
+
+    colores: yup
+        .array()
+        .of(
+            yup
+                .number()
+                .required()
+        )
+        .typeError('Seleccione al menos un color')
+        .required('El color es requerido'),
 });
