96d868443ab32951a58019897320d8b38c12905e
franmorooliacci <franmorooliacci@gmail.com>
2025-07-08 13:08:27 -0300

    refactor: remove unnecessary import

--- backend/src/urls.py
+++ backend/src/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView, TokenRefreshView)
+from rest_framework_simplejwt.views import TokenRefreshView
 from .views import (
     ResponsableViewSet, AnimalViewSet, RazaViewSet, 
     EfectorViewSet, AtencionViewSet, InsumoViewSet, 
