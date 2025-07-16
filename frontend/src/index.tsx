2c2ef25545ab8599a6508156f44e0dd6a71092e3
franmorooliacci <franmorooliacci@gmail.com>
2025-07-16 13:31:03 -0300

    refactor: migrate to ts/tsx

---
+++ frontend/src/index.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import theme from './theme';
+import './styles/index.css';
+import { AuthProvider } from './context/AuthContext';
+
+const rootElement = document.getElementById('root');
+if (rootElement) {
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </ThemeProvider>
+  );
+}
+
+reportWebVitals();
