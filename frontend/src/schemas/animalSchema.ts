feeca51ecc671c83a778cdea7f006a570ab2cd26
franmorooliacci <franmorooliacci@gmail.com>
2025-07-17 13:23:14 -0300

    refactor: migrate animalSchema.js to Typescript

---
+++ frontend/src/schemas/animalSchema.ts
@@ -0,0 +1,47 @@
+import * as yup from 'yup';
+
+export const animalSchema = yup.object({
+	nombre: yup
+		.string()
+		.required('Ingrese el nombre'),
+
+	sexo: yup
+		.string()
+		.oneOf(['M', 'H'], 'Seleccione el sexo')
+		.required(),
+
+	fecha_nacimiento: yup
+		.date()
+		.nullable(),
+
+	id_tamaño: yup
+		.number()
+		.required('Seleccione el tamaño'),
+
+	id_raza: yup
+		.number()
+		.required('Seleccione la raza'),
+
+	fallecido: yup
+		.number()
+		.oneOf([0, 1])
+		.required(),
+
+	esterilizado: yup
+		.number()
+		.oneOf([0, 1])
+		.required(),
+
+	adoptado_imusa: yup
+		.number()
+		.oneOf([0, 1])
+		.required(),
+
+	colores: yup
+		.array()
+		.of(yup.number().required())
+		.min(1, 'Seleccione al menos un color')
+		.required('El color es requerido'),
+});
+
+export type AnimalFormValues = yup.InferType<typeof animalSchema>;
