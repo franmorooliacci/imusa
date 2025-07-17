94850205cad8fef46a948c2f0e2670ff40451e57
franmorooliacci <franmorooliacci@gmail.com>
2025-07-17 13:24:29 -0300

    refactor: migrate responsableNoRnpSchema.js to Typescript

---
+++ frontend/src/schemas/responsableNoRnpSchema.ts
@@ -0,0 +1,39 @@
+import * as yup from 'yup';
+import { domicilioSchema } from './domicilioSchema';
+import { contactoSchema } from './contactoSchema';
+
+export const responsableNoRnpSchema = yup
+	.object({
+		nombre: yup
+			.string()
+			.required('Debe completar el nombre'),
+
+		apellido: yup
+			.string()
+			.required('Debe completar el apellido'),
+
+		dni: yup
+			.number()
+			.typeError('El DNI debe ser un número')
+			.integer('El DNI debe ser un número entero')
+			.positive('El DNI debe ser positivo')
+			.required('Debe completar el DNI'),
+
+		sexo: yup
+			.string()
+			.oneOf(['M', 'F'], 'Seleccione el sexo')
+			.required('Debe seleccionar el sexo'),
+
+		fecha_nacimiento: yup
+			.date()
+			.typeError('Fecha inválida')
+			.max(new Date(), 'Fecha inválida')
+			.required('Debe completar la fecha de nacimiento'),
+
+		domicilioActual: domicilioSchema.required('Debe completar domicilio actual'),
+
+		contacto: contactoSchema.required()
+	})
+	.required();
+
+export type ResponsableNoRnpFormValues = yup.InferType<typeof responsableNoRnpSchema>;
