f6f0cc3ade027decbf1e1ea48bf59388f204fa6c
franmorooliacci <franmorooliacci@gmail.com>
2025-07-23 13:58:07 -0300

    feat: add helper function to format domicilio obj according to db requirements

---
+++ frontend/src/features/persona/utils/format-domicilio.ts
@@ -0,0 +1,26 @@
+import { DomicilioDTO } from '@common/types/dto';
+
+// Formatea domicilio para agregar a la db
+export const formatDomicilio = (domicilio: Record<string, any>): DomicilioDTO => {
+
+    return {
+        calle: String(domicilio.calle),
+        codigo_calle: domicilio.codigo_calle === '' ? null : Number(domicilio.codigo_calle) || null,
+        altura: Number(domicilio.altura),
+        bis: domicilio.bis ? 1 : 0,
+        letra: domicilio.letra === '' ? null : String(domicilio.letra),
+        piso: domicilio.piso === '' ? null : Number(domicilio.piso) || null,
+        depto: domicilio.depto === '' ? null : String(domicilio.depto),
+        monoblock: domicilio.monoblock === '' ? null : Number(domicilio.monoblock) || null,
+        localidad: String(domicilio.localidad),
+        barrio: domicilio.barrio === '' ? null : String(domicilio.barrio),
+        vecinal: domicilio.vecinal === '' ? null : String(domicilio.vecinal),
+        distrito: domicilio.distrito === '' ? null : String(domicilio.distrito),
+        seccional_policial: domicilio.seccional_policial === '' ? null : String(domicilio.seccional_policial),
+        coordenada_x: domicilio.coordenada_x === '' ? null : String(domicilio.coordenada_x),
+        coordenada_y: domicilio.coordenada_y === '' ? null : String(domicilio.coordenada_y),
+        fraccion_censal: domicilio.fraccion_censal === '' ? null : String(domicilio.fraccion_censal),
+        radio_censal: domicilio.radio_censal === '' ? null : String(domicilio.radio_censal),
+        lineas_tup: domicilio.lineas_tup === '' ? null : String(domicilio.lineas_tup),
+    };
+};
