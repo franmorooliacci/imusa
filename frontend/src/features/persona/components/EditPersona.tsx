1d37a58d14f283d5d3cec251d58b6bb48b533087
franmorooliacci <franmorooliacci@gmail.com>
2025-07-24 08:12:50 -0300

    refactor: rename and migrate EditResponsable.jsx to Typescript

---
+++ frontend/src/features/persona/components/EditPersona.tsx
@@ -0,0 +1,172 @@
+import React, { useState } from 'react';
+import { Box, Button } from '@mui/material';
+import { useForm, FormProvider } from 'react-hook-form';
+import * as yup from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { Setter, Persona } from '@common/types';
+import { updatePersona } from '@features/persona/services/persona-api';
+import { domicilio, contacto } from '@features/persona/schemas';
+import { formatDomicilio, domicilioExists } from '@features/persona/utils';
+import DomicilioForm from './DomicilioForm';
+import ContactoForm from './ContactoForm';
+
+type Props = {
+    editDomicilio: boolean;
+    setEditDomicilio: Setter<boolean>;
+    editContacto: boolean;
+    setEditContacto: Setter<boolean>;
+    existingPersona: Persona;
+    setExistingPersona: Setter<Persona>;
+    setAlertOpen: Setter<boolean>;
+    setAlertMsg: Setter<string>;
+    setAlertSeverity: Setter<string>;
+};
+
+const EditPersona = (props: Props) => {
+    const {
+        editDomicilio,
+        setEditDomicilio,
+        editContacto,
+        setEditContacto,
+        existingPersona,
+        setExistingPersona,
+        setAlertOpen,
+        setAlertMsg,
+        setAlertSeverity
+    } = props;
+    const nestedContactoSchema = yup.object({ contacto: contacto.required() });
+    const nestedDomicilioSchema = yup.object({ domicilio: domicilio.required() });
+    const contactoMethods = useForm({
+        mode: 'onChange',
+        resolver: yupResolver(nestedContactoSchema),
+        defaultValues: {
+            contacto: { 
+                telefono: existingPersona.telefono ?? '', 
+                mail: existingPersona.mail ?? '' 
+            }
+        }
+    });
+    const { handleSubmit: handleSubmitContacto } = contactoMethods;
+    const domicilioMethods = useForm({
+        mode: 'onChange',
+        resolver: yupResolver(nestedDomicilioSchema),
+        defaultValues: {
+            domicilio: existingPersona.domicilio_actual ?? {}
+        }
+    });
+    const { handleSubmit: handleSubmitDomicilio } = domicilioMethods;
+    const [domicilioDone, setDomicilioDone] = useState<boolean>(false);
+
+    const onSubmitContacto = async (data: Record<string, any>): Promise<void> => {
+        try{
+            await updatePersona(existingPersona.id, data.contacto);
+            
+            setExistingPersona((prev) => ({
+                ...prev,
+                telefono: data.contacto.telefono,
+                mail: data.contacto.mail,
+            }));
+
+            
+            setAlertSeverity('success');
+            setAlertMsg('Modificación realizada con éxito!');
+            setAlertOpen(true);
+            
+            setTimeout(() => {
+                setEditContacto(false);
+            }, 3000); // Timeout para que se muestre la alerta
+        } catch(error){
+            setAlertSeverity('error');
+            setAlertMsg('No se ha podido realizar la modificación.');
+            setAlertOpen(true);
+        }
+    };
+
+    const onSubmitDomicilio = async (data: Record<string, any>): Promise<void> => {
+        try{
+            const formattedDomicilio = formatDomicilio(data.domicilio);
+            const newDomicilio = await domicilioExists(formattedDomicilio);
+
+            // Actualizo los datos del responsable que voy a mostrar
+            setExistingPersona((prev) => ({
+                ...prev,
+                domicilio_actual: newDomicilio
+            }));
+
+            // Actualizo el nuevo domicilio en la db
+            await updatePersona(existingPersona.id, { id_domicilio_actual: newDomicilio.id });
+
+            setAlertSeverity('success');
+            setAlertMsg('Modificación realizada con éxito!');
+            setAlertOpen(true);
+            
+            setTimeout(() => {
+                setEditDomicilio(false);
+            }, 3000); // Timeout para que se muestre la alerta
+
+        } catch(error){
+            setAlertSeverity('error');
+            setAlertMsg('No se ha podido realizar la modificación.');
+            setAlertOpen(true);
+        }
+    };
+
+    return (
+        <Box>
+            {editContacto && (
+                <FormProvider {...contactoMethods}>
+                    <Box component='form' onSubmit={handleSubmitContacto(onSubmitContacto)} noValidate>
+                        <ContactoForm name = {'contacto'} />
+                        <Box sx={{ display: 'flex', justifyContent: 'center', gap: 1 }}>
+                            <Button 
+                                variant='outlined' 
+                                color='error' 
+                                onClick={() => setEditContacto(false)}
+                                size='small'
+                            >
+                                Cancelar
+                            </Button>
+                            <Button 
+                                type='submit' 
+                                variant='contained' 
+                                color='primary'
+                                size='small'
+                            >
+                                Guardar
+                            </Button>
+                        </Box>
+                    </Box>
+                </FormProvider>
+            )}
+
+            {editDomicilio && (
+                <FormProvider {...domicilioMethods}>
+                    <Box component='form' onSubmit={handleSubmitDomicilio(onSubmitDomicilio)} noValidate>
+                        <DomicilioForm name = {'domicilio'} title = {'Domicilio'} setDomicilioDone = {setDomicilioDone} />
+                        <Box sx={{ display: 'flex', justifyContent: 'center', gap: 1, mt: 2 }}>
+                            <Button 
+                                variant='outlined' 
+                                color='error' 
+                                onClick={() => setEditDomicilio(false)}
+                                size='small'
+                            >
+                                Cancelar
+                            </Button>
+                            <Button 
+                                type='submit' 
+                                variant='contained' 
+                                color='primary'
+                                size='small'
+                                disabled={!domicilioDone}
+                            >
+                                Guardar
+                            </Button>
+                        </Box>
+                    </Box>
+                </FormProvider>
+            )}
+        </Box>
+    );
+};
+
+export default EditPersona;
