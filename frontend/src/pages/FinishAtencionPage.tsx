ee76e4047452962a13c3ed87d0364428cdc07cf0
franmorooliacci <franmorooliacci@gmail.com>
2025-07-30 08:56:21 -0300

    refactor: migrate FinishAtencionPage.jsx to Typescript

---
+++ frontend/src/pages/FinishAtencionPage.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { FinishAtencion } from '@features/atencion';
+
+const FinishAtencionPage = () => {
+    return (
+        <FinishAtencion />
+    );
+};
+
+export default FinishAtencionPage;
