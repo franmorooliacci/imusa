18f497cbdd9eb9bd2d6a679daad97602d9f3cff5
franmorooliacci <franmorooliacci@gmail.com>
2025-07-30 08:55:19 -0300

    refactor: migrate AtencionesListPage.jsx to Typescript

---
+++ frontend/src/pages/AtencionesListPage.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { ListAtencion } from '@features/atencion';
+
+const AtencionesListPage = () => {
+    return (
+        <ListAtencion />
+    );
+};
+
+export default AtencionesListPage;
