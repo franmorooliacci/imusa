622a722adc586acabe182fb37bb7680756a43b8c
franmorooliacci <franmorooliacci@gmail.com>
2025-07-30 09:01:26 -0300

    refactor: migrate index.jsx to Typescript

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
-import '@common/styles/index.css';
-import { AuthProvider } from '@common/context/AuthContext';
+import { AuthProvider } from '@common/context/auth';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import './index.css';
 import theme from './theme';
 
 const rootElement = document.getElementById('root');
