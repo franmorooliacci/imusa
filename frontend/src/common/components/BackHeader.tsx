4db6fcace61b366f79945ef359518276056c46d1
franmorooliacci <franmorooliacci@gmail.com>
2025-07-31 09:09:30 -0300

    feat: extend BackHeader component to receive action buttons

--- frontend/src/common/components/BackHeader.tsx
+++ frontend/src/common/components/BackHeader.tsx
@@ -1,16 +1,17 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
-import Box from '@mui/material/Box';
-import IconButton from '@mui/material/IconButton';
+import { Box, IconButton, Tooltip } from '@mui/material';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
+import { useNavigate } from 'react-router-dom';
 
-const BackHeader = ({ navigateTo }: { navigateTo: string }) => {
+const BackHeader = ({ navigateTo, actions = [] }: { navigateTo: string, actions?: React.ReactNode[] }) => {
     const navigate = useNavigate();
 
     return (
         <Box
             sx={{
+                display: 'flex',
+                alignItems: 'center',
                 width: '100%',
                 bgcolor: 'background.paper',
                 p: 1,
@@ -18,20 +19,30 @@ const BackHeader = ({ navigateTo }: { navigateTo: string }) => {
                 borderRadius: 4
             }}
         >
-            <IconButton
-                onClick={() => navigate(navigateTo)}
-                sx={{
-                    ml: 1,
-                    color: (theme) => theme.palette.text.primary,
-                    '&:hover': {
-                        backgroundColor: 'rgba(0, 0, 0, 0.08)',
-                        transform: 'scale(1.1)',
-                        transition: 'transform 0.2s',
-                    },
-                }}
-            >
-                <FontAwesomeIcon icon={faArrowLeft} size="1x" />
-            </IconButton>
+            <Tooltip title='Volver' arrow>
+                <IconButton
+                    onClick={() => navigate(navigateTo)}
+                    sx={{
+                        ml: 1,
+                        color: (theme) => theme.palette.text.primary,
+                        '&:hover': {
+                            backgroundColor: 'rgba(0, 0, 0, 0.08)',
+                            transform: 'scale(1.1)',
+                            transition: 'transform 0.2s',
+                        },
+                    }}
+                >
+                    <FontAwesomeIcon icon={faArrowLeft} size='1x' />
+                </IconButton>
+            </Tooltip>
+
+            <Box sx={{ flexGrow: 1 }} />
+
+            {actions.map((btn, i) => (
+                <Box key={i} sx={{ mr: 1 }}>
+                    {btn}
+                </Box>
+            ))}
         </Box>
     );
 };
