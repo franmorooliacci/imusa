c94fa90bd7a69239b9e520f255f109bd40e4e7e6
franmorooliacci <franmorooliacci@gmail.com>
2025-07-31 09:22:06 -0300

    chore: clean imports

--- frontend/src/features/atencion/index.ts
+++ frontend/src/features/atencion/index.ts
@@ -4,5 +4,4 @@ export { default as ViewAtencion } from './components/ViewAtencion';
 export { default as FinishAtencion } from './components/FinishAtencion';
 export type { Atencion, Personal } from './types';
 export { getAtenciones, sendInformeEmail } from './api';
-export { createEmptyAtencion } from './utils/create-empty-atencion';
-export { sortAtencionesAsc } from './utils/sort-atenciones-asc';
+export * from './utils';
