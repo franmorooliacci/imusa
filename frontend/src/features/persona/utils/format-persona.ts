8af709a7d5424d86b7cca0441eea0ab8e0da24ff
franmorooliacci <franmorooliacci@gmail.com>
2025-08-05 13:28:48 -0300

    feat: add helper function to format persona object according to db requirements

---
+++ frontend/src/features/persona/utils/format-persona.ts
@@ -0,0 +1,18 @@
+import { PersonaDTO } from '../types';
+
+export const formatPersona = (data: Record<string, any>): PersonaDTO => {
+    const date = data.fecha_nacimiento ? new Date(data.fecha_nacimiento) : null;
+    const formattedDate = date ? date.toISOString().split('T')[0] : '';
+
+    return { 
+        nombre: data.nombre,
+        apellido: data.apellido,
+        dni: data.dni,
+        sexo: data.sexo,
+        fecha_nacimiento: formattedDate,
+        id_domicilio_renaper: data.id_domicilio_renaper,
+        id_domicilio_actual: data.id_domicilio_actual,
+        telefono: data.telefono,
+        mail: data.mail === '' ? null : data.mail
+    };
+};
