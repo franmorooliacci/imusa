a58b9de6c2e964228cd9eda29a3de314be26d164
franmorooliacci <franmorooliacci@gmail.com>
2025-12-18 09:47:27 -0300

    feat: add url paths for React files

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,5 +1,10 @@
-from django.urls import path, include
+from django.urls import path, re_path, include
+from django.views.generic import TemplateView, RedirectView
+from django.templatetags.static import static
 
 urlpatterns = [
     path('api/', include('src.urls')),
+    path('favicon.ico', RedirectView.as_view(url=static('favicon.ico'), permanent=True)),
+    path('', TemplateView.as_view(template_name='index.html')),
+    re_path(r'^(?!api/|static/).*$', TemplateView.as_view(template_name='index.html')),
 ]
